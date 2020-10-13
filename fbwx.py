a8b47d8b3c5ee9bd684a75ad59f042a43578db7d
Eli Heuer <elih@protonmail.com>
2020-10-12 22:14:44 -0700

    Refactor script

--- fbwx.py
+++ fbwx.py
@@ -2,54 +2,46 @@
 import os
 import wx
 
+
 class FileDrop(wx.FileDropTarget):
 
     def __init__(self, window):
         wx.FileDropTarget.__init__(self)
         self.window = window
+        self.window.WriteText("Drag & drop a font file here, then look for a markdown report on your desktop.")
 
     def OnDropFiles(self, x, y, filenames):
         for fontpath in filenames:
             try:
                 print('fontpath = ',fontpath)
-                print('fontbakery check-googlefonts ' + fontpath + ' -l WARN --ghmarkdown ~/Desktop/fontbakery-report.md')
                 os.system('fontbakery check-googlefonts ' + fontpath + ' -l WARN --ghmarkdown ~/Desktop/fontbakery-report.md')
-                #file = open(name, 'r')
-                #text = file.read()
-                #self.window.WriteText(text)
             except IOError as error:
                 msg = "Error opening file\n {}".format(str(error))
                 dlg = wx.MessageDialog(None, msg)
                 dlg.ShowModal()
                 return False
-            except UnicodeDecodeError as error:
-                msg = "Cannot open non ascii files\n {}".format(str(error))
-                dlg = wx.MessageDialog(None, msg)
-                dlg.ShowModal()
-                return False
-            finally:
-                print("Done :-)")
-                #file.close()
         return True
 
-class Example(wx.Frame):
+
+class FontBakeryGUI(wx.Frame):
 
     def __init__(self, *args, **kw):
-        super(Example, self).__init__(*args, **kw)
+        super(FontBakeryGUI, self).__init__(*args, **kw)
         self.InitUI()
 
     def InitUI(self):
         self.text = wx.TextCtrl(self, style = wx.TE_MULTILINE)
         dt = FileDrop(self.text)
         self.text.SetDropTarget(dt)
-        self.SetTitle('File drag and drop')
+        self.SetTitle('Font Bakery Desktop GUI')
         self.Centre()
+
 
 def main():
 
     app = wx.App()
-    ex = Example(None)
-    ex.Show()
+    fb = FontBakeryGUI(None)
+    fb.Show()
     app.MainLoop()
 
 if __name__ == '__main__':
