9d47973f6c63f899574ce72060e92081ce397076
Eli Heuer <elih@protonmail.com>
2020-10-14 19:43:30 -0700

    Format main script with Black

--- fbwx.py
+++ fbwx.py
@@ -4,16 +4,24 @@ import wx
 
 
 class FileDrop(wx.FileDropTarget):
-
     def __init__(self, window):
         wx.FileDropTarget.__init__(self)
         self.window = window
-        self.window.WriteText("Drag & drop a font file here, then look for a markdown report on your desktop.")
+        self.window.WriteText(
+            "Drag & drop a font file here, then look for a markdown report on your desktop."
+        )
 
     def OnDropFiles(self, x, y, filenames):
         for fontpath in filenames:
             try:
-                subprocess.call(['fontbakery check-googlefonts ' + fontpath + ' --ghmarkdown ~/Desktop/fontbakery-report.md'], shell=True)
+                subprocess.call(
+                    [
+                        "fontbakery check-googlefonts "
+                        + fontpath
+                        + " --ghmarkdown ~/Desktop/fontbakery-report.md"
+                    ],
+                    shell=True,
+                )
             except IOError as error:
                 msg = "Error opening file\n {}".format(str(error))
                 err = wx.MessageDialog(None, msg)
@@ -23,16 +31,15 @@ class FileDrop(wx.FileDropTarget):
 
 
 class FontBakeryGUI(wx.Frame):
-
     def __init__(self, *args, **kw):
         super(FontBakeryGUI, self).__init__(*args, **kw)
         self.InitUI()
 
     def InitUI(self):
-        self.text = wx.TextCtrl(self, style = wx.TE_MULTILINE)
+        self.text = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         dt = FileDrop(self.text)
         self.text.SetDropTarget(dt)
-        self.SetTitle('Font Bakery Desktop GUI')
+        self.SetTitle("Font Bakery Desktop GUI")
         self.Centre()
 
 
@@ -42,5 +49,6 @@ def main():
     fb.Show()
     app.MainLoop()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
